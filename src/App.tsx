d6beb3c3d33ac0fb9589ebfa3af09347c6d34c7f
unknown <marksgilligan@gmail.com>
2021-05-06 23:05:08 +0100

    reafactored the app into an iff statement to better handle the different variants that render based on the auth progress

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ import LandingPage from './pages/LandingPage/LandingPage'
 
 const App: React.FC = () => {
 //Create formState to handle the users authentication path
-const [authState, setAuthState] = useState<string>('')
+const [authState, setAuthState] = useState<string>('loading')
 
 async function isUserSignedIn(){
   const userCredentials = await Auth.currentUserCredentials()
@@ -52,45 +52,55 @@ async function isUserSignedIn(){
 }
 isUserSignedIn()
 
+function appRender(){
+  if(authState === 'loading'){
+    return (
+      <div>LOADING</div>
+    )
+  } else if (authState !== 'signedIn'){
+    return (
+      <LandingPage authState={authState} setAuthState={setAuthState}/>
+    ) 
+  } else return(
+    <IonReactRouter>
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route exact path="/tab1">
+          <Tab1 />
+        </Route>
+        <Route exact path="/tab2">
+          <Tab2 />
+        </Route>
+        <Route path="/tab3">
+          <Tab3 />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/tab1" />
+        </Route>
+      </IonRouterOutlet>
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="tab1" href="/tab1">
+          <IonIcon icon={triangle} />
+          <IonLabel>Tab 1</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab2" href="/tab2">
+          <IonIcon icon={ellipse} />
+          <IonLabel>Tab 2</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab3" href="/tab3">
+          <IonIcon icon={square} />
+          <IonLabel>Tab 3</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+  </IonReactRouter>
+  )
+}
+
 
 return(
 <IonApp>
-  { authState !== 'signedIn' ? 
-    <LandingPage authState={authState} setAuthState={setAuthState}/>
-  :
-   <IonReactRouter>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Route exact path="/tab1">
-              <Tab1 />
-            </Route>
-            <Route exact path="/tab2">
-              <Tab2 />
-            </Route>
-            <Route path="/tab3">
-              <Tab3 />
-            </Route>
-            <Route exact path="/">
-              <Redirect to="/tab1" />
-            </Route>
-          </IonRouterOutlet>
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="tab1" href="/tab1">
-              <IonIcon icon={triangle} />
-              <IonLabel>Tab 1</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab2" href="/tab2">
-              <IonIcon icon={ellipse} />
-              <IonLabel>Tab 2</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab3" href="/tab3">
-              <IonIcon icon={square} />
-              <IonLabel>Tab 3</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </IonReactRouter>
-    }
+{appRender()}
 </IonApp>
 )
 };
