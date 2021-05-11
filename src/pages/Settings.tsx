7e11c1ed1c82fe73c029462f4c6b228d477ef3b3
unknown <marksgilligan@gmail.com>
2021-05-11 11:02:09 +0100

    redux tutorial added

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,12 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonHeader, IonPage } from '@ionic/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../redux/counter';
+
 import './Settings.css';
 
 const Settings: React.FC = () => {
+
+  const { count } = useSelector( (state: any) => state.counter )
+  const dispatch = useDispatch()
+console.log(count);
+
   return (
     <IonPage>
       <div className='settingsContainer'>
         <IonHeader></IonHeader>
+        <div>
+          <div>{count}</div>
+          <button onClick={()=> dispatch(increment())}>ADD</button>
+          <button onClick={()=> dispatch(decrement())}>SUBTRACT</button>
+        </div>
       </div>
     </IonPage>
   );
