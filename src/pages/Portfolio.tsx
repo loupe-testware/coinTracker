c17682ff88a9f860146063a883997fe114ae03b3
Mark <marksgilligan@gmail.com>
2021-07-05 17:18:44 +0100

    added reordering and styling to portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,21 +1,39 @@
-import { IonContent, IonPage, IonRefresher, IonRefresherContent, IonSlides, IonSlide } from '@ionic/react';
+import { IonContent, IonPage, IonRefresher, IonRefresherContent, IonSlides, IonSlide, IonReorder, IonReorderGroup } from '@ionic/react';
+import { RefresherEventDetail, ItemReorderEventDetail } from '@ionic/core'
+import { useSelector, useDispatch } from 'react-redux';
+import {getCoins} from '../redux/coinSlice'
+
+import { coinInterface, coinsStoreInterface } from '../interfaces/interfaces'
 
 import dummyData from '../test.json'
 
 import './Portfolio.css';
 
 const Portfolio: React.FC = () => {
+  const { payload } = useSelector((state: coinsStoreInterface) => state.coins.list)
+  const dispatch = useDispatch()
 
+  //slide options and speed
   const slideOpts = {
     initialSlide: 1,
     speed: 400
   };
   
+  function onRefresh(event: CustomEvent<RefresherEventDetail>){
+    dispatch(getCoins())
+    event.detail.complete();
+  }
+
+  function doReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    event.detail.complete();
+  }
+
+  console.log(payload);
 
   return (
     <IonPage>
       <IonContent className='portfolioMainContainer'>
-        <IonRefresher slot='fixed'>
+        <IonRefresher slot='fixed' onIonRefresh={onRefresh}>
           <IonRefresherContent>
           </IonRefresherContent>
         </IonRefresher>
@@ -23,13 +41,50 @@ const Portfolio: React.FC = () => {
           {
             dummyData.portfolios.map((item, index)=>{
               return (
-              <IonSlide>
+              <IonSlide className='portfolioSlideContainer'>
+                <div className='portfolioSlide'>
                 <div className='portfolioTotalContainer'>
                   <div className='portfolioName'>{item.portfolio_name}</div>
                   <div className='portfolioSettings'>...</div>
-                  <div className='portfolioValue'>{item.coins.map((coin, index)=>{        
-                      return <div>{coin.totalQuantity}</div>
-                  })}</div>
+                  <div className='portfolioValue'>
+                    £VALUE
+                  </div>
+                </div>
+                <div className='portfolioCoinsContainer'>
+                  <IonReorderGroup disabled={false} onIonItemReorder={doReorder}>
+                    {
+                      item.coins.map((coin, index)=>{
+                        const coinData = payload.filter((item)=>{
+                          if (coin.coin_name === item.id){
+                            return item
+                          }
+                        })
+
+                        const coinTotal = coin.transactions.reduce((prev, cur)=>{
+                          return prev + cur.quantity;
+                        }, 0)
+                        
+                        return(
+                          <IonReorder>
+                            <div className="portfolioCoinContainer">
+                              <img className="portfolioCoinLogo" src={coinData[0].image} alt='coin logo'/>
+                              <div className="portfolioCoinSymbol">
+                              {coinData[0].symbol.toUpperCase()}
+                              </div>
+                              <div className="portfolioCoinCurrentPrice">
+                                {coinData[0].current_price}
+                              </div>
+                              <div className="portfolioCoinValue">
+                              {coinTotal}
+                              </div>
+                            </div>
+                          </IonReorder>
+                        )
+                      })
+                    }
+                   
+                  </IonReorderGroup>
+                </div>
                 </div>
               </IonSlide>
               )
